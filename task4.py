0a05c59cadd25f3034eab32e5007e39ad90c058c
Sebkd <sebkd@mail.ru>
2021-01-09 01:55:55 +0700

    coding task4

--- task4.py
+++ task4.py
@@ -6,25 +6,20 @@
 Пример исходного списка: [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11].
 Результат: [23, 1, 3, 10, 4, 11]
 '''
-import random
+from random import randint, shuffle
+from functools import reduce
 
 def check_fun(*args):
-    for count, element in enumerate(args[0]):
-        yield element
+    print(args)
+    pass
 
-original_list = [element for element in range(random.randint(1, 10))]
-add_list = [element for element in range(random.randint(1, 10))]
+original_list = [element for element in range(randint(1, 10))]
+add_list = [element for element in range(randint(1, 10))]
 original_list.extend(add_list)
-random.shuffle(original_list)
+shuffle(original_list)
 print(len(original_list), original_list)
-add_number = check_fun(original_list)
-print(add_number)
-# for count, element in enumerate (original_list):
-#     for number in original_list[count:]:
-#         print (count, element)
-#         if original_list.index(element, count) != 0:
-#             print(f' попались {count}, {element}')
-#             add_list.append(element)
-
-
+add_list.clear()
+#add_list = reduce(lambda a, b: a if a > b else b, original_list)
+add_list = map(check_fun, original_list)
 print(add_list)
+
