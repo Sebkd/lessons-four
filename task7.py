195e4ff85d3b4f8ef6f1ac4e4a0ef2b9fa34cd19
Sebkd <sebkd@mail.ru>
2021-01-09 14:10:22 +0700

    final task7

--- task7.py
+++ task7.py
@@ -6,3 +6,14 @@
 начиная с 1! и до n!.
 Подсказка: факториал числа n — произведение чисел от 1 до n. Например, факториал четырёх 4! = 1 * 2 * 3 * 4 = 24.
 '''
+from math import factorial
+from random import randint
+
+def fact(end_number):
+    for el in range(1, end_number+1):
+        print(f'Факториал {el}! = {factorial(el)}')
+    yield factorial(end_number)
+    
+calc_number = randint(1,25)
+get_fact = fact(calc_number)
+print(f'\nЧисло n = {calc_number}, Факториал n! = {[el for el in get_fact]}, Проверка = {factorial(calc_number)}')
