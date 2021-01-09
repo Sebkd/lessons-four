4cb526ef9ebe05f50c5c56a715e6bea2120d49ce
Sebkd <sebkd@mail.ru>
2021-01-09 10:18:13 +0700

    final task4

--- task4.py
+++ task4.py
@@ -7,21 +7,9 @@
 Результат: [23, 1, 3, 10, 4, 11]
 '''
 from random import randint, shuffle
-from functools import reduce
 
-def check_fun(*args):
-    print(args)
-    pass
-
-original_list = [element for element in range(randint(1, 10))]
-add_list = [element for element in range(randint(1, 10))]
-original_list.extend(add_list)
+original_list = [element for element in range(randint(1, randint(10, 15)))] + \
+                [element for element in range(randint(1, randint(5, 10)))]
 shuffle(original_list)
-print(original_list)
-add_list.clear()
-for count, element in enumerate(original_list, start = 1):
-    add_list = list(filter(lambda number: number != element, original_list[count:]))
-    print(count, element)
-#add_list = [element for element in original_list if (filter(lambda number: number != element, original_list))]
-print(add_list)
-
+print(f'Original list = {original_list} \n'
+      f'Changed list = {[el for el in original_list if original_list.count(el) == 1]} \n')
